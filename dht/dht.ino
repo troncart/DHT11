389b52e7940b1a8f007fd33db3d783a2f6af73fa
Troncart <47349813+troncart@users.noreply.github.com>
2019-07-29 17:21:24 +0530

    DHT11 sensor

---
+++ dht/dht.ino
@@ -0,0 +1,38 @@
+/*********TACHLOG powered********
+*********www.troncart.com*******
+**electronics components online**
+***********DHT 11**************
+***********29-07-2019**************
+*/
+#include "dht.h"
+#define dht_apin A0 // Analog Pin sensor is connected to
+ 
+dht DHT;
+ 
+void setup()
+{
+ 
+  Serial.begin(9600);
+  delay(500);//Delay to let system boot
+  Serial.println("DHT11 Humidity & temperature Sensor\n\n");
+  delay(1000);//Wait before accessing Sensor
+ 
+}
+ 
+void loop()
+ {
+   
+ 
+    DHT.read11(dht_apin);
+    
+    Serial.print("Current humidity = ");
+    Serial.print(DHT.humidity);
+    Serial.print("%  ");
+    Serial.print("temperature = ");
+    Serial.print(DHT.temperature); 
+    Serial.println("C  ");
+    delay(5000);//Wait 5 seconds before accessing sensor again.
+ 
+    //Fastest should be once every two seconds.
+ 
+ } 
